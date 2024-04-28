bdc8ace65096714573c1bdb6b7c1ca0a673d7b97
pedcasjim1 <pedcasjim1@alum.us.es>
2024-04-28 11:59:59 +0200

    prueba 1

--- app/src/main/java/us/dit/Control.java
+++ app/src/main/java/us/dit/Control.java
@@ -2,14 +2,14 @@ package us.dit;
 
 public class Control {
    public static void main(String[] args) {
-    	int var1=2;
-    	int var2=5;        
+    	int var1=2233333333333333333333333;
+    	int var2=22333333333333335        
         int resultado = operacion(var1,var2);        
         System.out.println(us.dit.Validador.validar(resultado));       
         }
         
    public static int operacion(int a, int b) {
-        return a*b;
+        return a+b;
    }    
   
 }
