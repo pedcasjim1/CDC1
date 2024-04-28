ee5275adbbcf6be932e609ae08583974ef40b676
pedcasjim1 <pedcasjim1@alum.us.es>
2024-04-28 12:07:33 +0200

    Añadido comentario para diferenciar las versiones

--- app/src/main/java/us/dit/Control.java
+++ app/src/main/java/us/dit/Control.java
@@ -1,5 +1,7 @@
 package us.dit;
 
+
+//hola esto es un comentario para estudiar
 public class Control {
    public static void main(String[] args) {
     	int var1=2221;
@@ -7,7 +9,7 @@ public class Control {
         int resultado = operacion(var1,var2);        
         System.out.println(us.dit.Validador.validar(resultado));       
         }
-        
+       
    public static int operacion(int a, int b) {
         return a/b;
    }    
